2d0d101868708663a470c921c8af9bc879aa6106
vinicius <viniciusjlc@gmail.com>
2025-02-26 18:42:04 -0300

    - Commit resolvido

--- src/exemplo/Main.java
+++ src/exemplo/Main.java
@@ -1,9 +1,20 @@
 package exemplo;
 
+import exemplo.classes.ControleRemoto;
+import exemplo.classes.ImplementaPanasonic;
+import exemplo.classes.ImplementaPhilco;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		
+		ControleRemoto controle = new ControleRemoto();
+		controle.setImplementacao(new ImplementaPanasonic());
+		controle.ligarTV();
+		
+		//Mudou de TV
+		controle.setImplementacao(new ImplementaPhilco());
+		controle.ligarTV();
 
 	}
 
